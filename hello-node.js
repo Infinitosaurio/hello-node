369f3e49db2df354ad1730786d7e0702ba3e30c0
JackLondon84 <vishalsa84@gmail.com>
2014-03-27 10:22:31 +0100

    archivo hello-node añadido

---
+++ hello-node.js
@@ -0,0 +1,15 @@
+var net = require('net');
+
+var server = net.createServer(function(connection) {
+    console.log('Connection open');
+    connection.write('Hello?');
+    connection.on('data', function(data) {
+        if (String(data).trim() != 'hello') {
+            connection.write('ERROR');
+        } else {
+            connection.write('world');
+            console.log('Connection closed');
+        }
+    });
+});
+server.listen(port);
